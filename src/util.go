c10f2f3e2ae329392fb4842a6b6b252f150b9775
rajthilakrv85 <77279121+rajthilakrv85@users.noreply.github.com>
2026-07-03 09:59:02 +0530

    Add files via upload

---
+++ src/util.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"net"
+	"strings"
+	"crypto/sha1"
+	"encoding/hex"
+
+	"github.com/google/uuid"
+)
+
+func CreateBranch() (string, error) {
+	uuid, err := uuid.NewRandom()
+	if err == nil {
+		tmp := strings.Split(uuid.String(), "-")
+		return "z9hG4bK" + tmp[len(tmp)-1], nil
+	}
+	return "", err
+}
+
+func CreateBranchFromSeed(seed string) (string, error) {
+	h := sha1.Sum([]byte(seed))
+	return "z9hG4bK" + hex.EncodeToString(h[:])[:16], nil
+}
+
+func CreateTag() (string, error) {
+	uuid, err := uuid.NewRandom()
+	if err == nil {
+		tmp := strings.Split(uuid.String(), "-")
+		return tmp[len(tmp)-1], nil
+	}
+	return "", err
+}
+
+func inStrArray(s string, a []string) bool {
+	for _, t := range a {
+		if t == s {
+			return true
+		}
+	}
+	return false
+}
+
+func strArraySub(a1 []string, a2 []string) []string {
+	r := make([]string, 0)
+	for _, s := range a1 {
+		if !inStrArray(s, a2) {
+			r = append(r, s)
+		}
+	}
+	return r
+}
+
+/*
+func splitAddr(addr string) (hostname string, port string, err error) {
+	pos := strings.LastIndex(addr, ":")
+	if pos == -1 {
+		err = errors.New("not a valid address")
+	} else {
+		hostname = addr[0:pos]
+		port = addr[pos+1:]
+		err = nil
+	}
+	return
+
+}*/
+
+func isIPAddress(addr string) bool {
+	if strings.HasPrefix(addr, "[") && strings.HasSuffix(addr, "]") {
+		addr = addr[1 : len(addr)-1]
+	}
+	return net.ParseIP(addr) != nil
+}
+
+func isIPv6(ip string) bool {
+	return strings.Contains(ip, ":")
+}
